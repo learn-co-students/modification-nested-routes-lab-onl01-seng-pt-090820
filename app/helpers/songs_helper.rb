d803026c0481d6f45f423f9341f2e95cb2edf1f1
DavidRMorphew <morphewd@umich.edu>
2021-02-05 09:27:37 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def add_new_song_link
+        if @artist
+            link_to("Add New Song", new_artist_song_path(@artist))
+        else
+            link_to("Add New Song", new_song_path)
+        end
+    end
 end
